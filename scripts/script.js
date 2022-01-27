979c4d46d73645e4b396a7a33dd50f344da398c9
VictorLe <victortrum1995@gmail.com>
2022-01-27 11:25:05 +0300

    Rename classes Change button to svg Fix script

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,17 @@
-let buttonEdit = document.querySelector(".button_edit");
-let buttonClose = document.querySelector(".button_close");
+let buttonEdit = document.querySelector(".profile__button_edit");
+let buttonClose = document.querySelector(".popup__button_close");
 let popup = document.querySelector(".popup");
 
+let formElement = document.querySelector(".popup__container");
+
+let nameInput = document.getElementById('name');
+let jobInput = document.getElementById('about-us');
+let profileName = document.querySelector(".profile__name");
+let profileJob = document.querySelector(".profile__about-us");
+
 function openPopup() {
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
     popup.classList.add("popup_opened");
 }
 
@@ -13,20 +22,11 @@ function closePopup() {
 buttonEdit.addEventListener("click", openPopup);
 buttonClose.addEventListener("click", closePopup);
 
-let formElement = document.querySelector(".popup__container");
-
-let nameInput = document.getElementById('name');
-let jobInput = document.getElementById('about-us');
-let profileName = document.querySelector(".profile__name");
-let profileJob = document.querySelector(".profile__about-us");
-
 function formSubmitHandler(evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
+    closePopup();
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
-
-let popupSaveClose = document.querySelector(".button_save");
-popupSaveClose.addEventListener("click", closePopup);
