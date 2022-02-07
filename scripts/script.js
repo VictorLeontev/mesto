2093b976ca84482a64d6b06b31980b62dbe1fffc
VictorLe <victortrum1995@gmail.com>
2022-02-07 18:15:37 +0300

    AddAnimation

--- scripts/script.js
+++ scripts/script.js
@@ -1,53 +1,43 @@
-let buttonEdit = document.querySelector(".profile__button-edit");
-let buttonCloseProfile = document.querySelector("#buttonCloseProfile");
-let popupProfile = document.querySelector("#profileedit");
+const buttonEdit = document.querySelector(".profile__button-edit");
+const buttonCloseProfile = document.querySelector("#buttonCloseProfile");
+const popupProfile = document.querySelector("#profileedit");
 
-let formElementProfile = document.querySelector("#profilePopup");
+const formElementProfile = document.querySelector("#profilePopup");
 
-let nameInput = document.getElementById('name');
-let jobInput = document.getElementById('about-us');
-let profileName = document.querySelector(".profile__name");
-let profileJob = document.querySelector(".profile__about-us");
+const nameInput = document.getElementById('name');
+const jobInput = document.getElementById('about-us');
+const profileName = document.querySelector(".profile__name");
+const profileJob = document.querySelector(".profile__about-us");
 
-function openPopupProfile() {
+/*function openPopupProfile() {
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
     popupProfile.classList.add("popup_opened");
-}
-
-function closePopupProfile() {
-    popupProfile.classList.remove("popup_opened");
-}
-
+}*/
 
 function formSubmitHandlerProfile(evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
-    closePopupProfile();
 }
 
-buttonEdit.addEventListener("click", openPopupProfile);
-buttonCloseProfile.addEventListener("click", closePopupProfile);
+buttonEdit.addEventListener("click", /*openPopupProfile*/ openPopup);
+buttonCloseProfile.addEventListener("click", closePopup);
 
 formElementProfile.addEventListener('submit', formSubmitHandlerProfile);
 
 /* функции для картинки */
 
-let buttonAdd = document.querySelector(".profile__button-add");
-let buttonClosePictures = document.querySelector("#buttonClosePicture");
-let popupPicture = document.querySelector("#pictureadding");
+const buttonAdd = document.querySelector(".profile__button-add");
+const buttonClosePictures = document.querySelector("#buttonClosePicture");
+const popupPicture = document.querySelector("#pictureadding");
 
-function openPopupPicture() {
+/*function openPopupPicture() {
     popupPicture.classList.add("popup_opened");
-}
+}*/
 
-function closePopupPicture() {
-    popupPicture.classList.remove("popup_opened");
-}
-
-buttonAdd.addEventListener("click", openPopupPicture);
-buttonClosePictures.addEventListener("click", closePopupPicture);
+buttonAdd.addEventListener("click", /*openPopupPicture*/ openPopup);
+buttonClosePictures.addEventListener("click", closePopup);
 
 const initialCards = [{
         name: 'Архыз',
@@ -78,12 +68,16 @@ const initialCards = [{
 const template = document.querySelector('.element__item').content;
 const elements = document.querySelector('.elements');
 
+const pictureFullPopup = document.getElementById("picturefullscreen");
+const pictureInfo = document.querySelector(".popup__image");
+const pictureSignature = document.querySelector(".popup__signature");
+
 function render() {
-    initialCards.forEach(function(card) { appendCard(card, true); });
+    initialCards.forEach(function(card) { appendCard(card /*, true*/ ); });
 };
 
-function appendCard(card, toBack) {
-    let cardElement = template.cloneNode(true);
+function appendCard(card /*, toBack*/ ) {
+    const cardElement = template.cloneNode(true);
     cardElement.querySelector('.element__text').textContent = card.name;
     cardElement.querySelector('.element__top').src = card.link;
     cardElement.querySelector('.element__top').alt = card.name;
@@ -100,9 +94,6 @@ function appendCard(card, toBack) {
     const pictureFull = cardElement.querySelector(".element__top");
     pictureFull.addEventListener("click",
         function(e) {
-            let pictureFullPopup = document.getElementById("picturefullscreen");
-            let pictureInfo = document.querySelector(".popup__image");
-            let pictureSignature = document.querySelector(".popup__signature")
             const cardElement = e.target.parentElement;
             pictureSignature.textContent = cardElement.querySelector('.element__top').alt;
             pictureInfo.src = cardElement.querySelector('.element__top').src;
@@ -111,40 +102,105 @@ function appendCard(card, toBack) {
         }
     );
 
-    if (toBack) {
+    /*if (toBack) {
         elements.append(cardElement);
     } else {
         elements.prepend(cardElement);
-    }
-
+    }*/
+    elements.append(cardElement);
 };
 
 render();
 
+function prependCard(card) {
+    const cardElement = template.cloneNode(true);
+    cardElement.querySelector('.element__text').textContent = card.name;
+    cardElement.querySelector('.element__top').src = card.link;
+    cardElement.querySelector('.element__top').alt = card.name;
+
+    const button = cardElement.querySelector(".element__button-delete");
+    button.addEventListener("click",
+        function(e) {
+
+            const cardElement = e.target.parentElement;
+
+            elements.removeChild(cardElement);
+        });
+
+    const pictureFull = cardElement.querySelector(".element__top");
+    pictureFull.addEventListener("click",
+        function(e) {
+            const cardElement = e.target.parentElement;
+            pictureSignature.textContent = cardElement.querySelector('.element__top').alt;
+            pictureInfo.src = cardElement.querySelector('.element__top').src;
+            pictureInfo.alt = cardElement.querySelector('.element__top').alt;
+            pictureFullPopup.classList.add("popup_opened");
+        }
+    );
+    elements.prepend(cardElement);
+}
+
 
 const pictureFullCloseButton = document.getElementById("pictureFullClose");
-pictureFullCloseButton.addEventListener("click",
-    function(e) {
-        let pictureFullPopup = document.getElementById("picturefullscreen");
-        pictureFullPopup.classList.remove("popup_opened");
-    });
-
+pictureFullCloseButton.addEventListener("click", closePopup);
 
 /*adding function*/
 
-let pictureName = document.getElementById('picturename');
-let url = document.getElementById('url');
+let pictureName = document.getElementById("picturename");
+let url = document.getElementById("url");
 
 let formElementPicture = document.querySelector("#picturePopup");
 
 function formSubmitHandlerPicture(evt) {
     evt.preventDefault();
-    let picture = new Object();
+    const picture = new Object();
     picture.name = pictureName.value;
     picture.link = url.value;
-    initialCards.unshift(picture);
-    appendCard(picture, false);
-    closePopupPicture();
+    /*appendCard(picture, false);*/
+    prependCard(picture);
+    document.addEventListener('animationend', function(e) {
+        if (e.animationName === 'fade-out') {
+            e.target.classList.remove('did-fade-in');
+        }
+    });
 }
 
 formElementPicture.addEventListener('submit', formSubmitHandlerPicture);
+
+const profileSaveButton = document.getElementById("profileSaveButton");
+const profileAddButton = document.getElementById("profileAddButton");
+profileSaveButton.addEventListener("click", closePopup); /* Когда вставляю функцию closePopup в функцию formSubmitHandler, evt.preventDefault мешает закрытию, передавая на e.target значение undefined. В данном же случае и preventDefault отрабатывает и closePopup так же корректно ведет себя */
+profileAddButton.addEventListener("click", closePopup);
+
+function closePopup(evt) {
+    let popups;
+
+    if (evt.target === buttonCloseProfile || evt.target === profileSaveButton) {
+        popups = popupProfile;
+    } else if (evt.target === buttonClosePictures || evt.target === profileAddButton) {
+        popups = popupPicture;
+    } else {
+        popups = pictureFullPopup;
+    }
+
+    popups.classList.remove("popup_opened");
+
+    //popups.classList.add("popup_closed");
+}
+
+function openPopup(evt) {
+
+    let popups;
+
+    if (evt.target === buttonEdit) {
+        profileName.textContent = nameInput.value;
+        profileJob.textContent = jobInput.value;
+        popups = popupProfile;
+    } else if (evt.target === buttonAdd) {
+        popups = popupPicture;
+    };
+
+    //popups.classList.remove("popup_closed");
+    popups.classList.add("popup_opened");
+}
+/*Беспорядок в обЪвлении переменных я устраню. Если я сейчас это сделаю, то просто запутаюсь где-что.*/
